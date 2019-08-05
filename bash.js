eb9ea4add962cd2827432393b634b8743ab927f9
Pelin Dogan <pelinsezgin@gmail.com>
2019-08-05 12:56:51 -0400

    PWD

---
+++ bash.js
@@ -0,0 +1,8 @@
+process.stdout.write('prompt > ');
+process.stdin.on('data',(data)=>{
+  const cmd = data.toString().trim();
+  if(cmd==='pwd'){
+    process.stdout.write(  process.cwd());
+  }
+  process.stdout.write('\nprompt > ');
+})
